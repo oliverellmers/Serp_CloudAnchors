efdbf6acc937416a11081e1105c8a113a2fe724d
Oliver Ellmers <oliver@random42.com>
2019-02-22 18:11:15 +0000

    Trying to move AndyManipulation into LocalPlayer

---
+++ Assets/GoogleARCore/Examples/CloudAnchors/Scripts/MyPlacementManipulator.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using GoogleARCore.Examples.ObjectManipulation;
+using UnityEngine.Networking;
+
+
+public class MyPlacementManipulator : Manipulator
+{
+    /// <summary>
+    /// The first-person camera being used to render the passthrough camera image (i.e. AR background).
+    /// </summary>
+    private Camera FirstPersonCamera;
+
+    /// <summary>
+    /// A model to place when a raycast from a user touch hits a plane.
+    /// </summary>
+    public GameObject AndyPrefab;
+
+    /// <summary>
+    /// Manipulator prefab to attach placed objects to.
+    /// </summary>
+    public GameObject ManipulatorPrefab;
+
+    /// <summary>
+    /// Returns true if the manipulation can be started for the given gesture.
+    /// </summary>
+    /// <param name="gesture">The current gesture.</param>
+    /// <returns>True if the manipulation can be started.</returns>
+    protected override bool CanStartManipulationForGesture(TapGesture gesture)
+    {
+        if (gesture.TargetObject == null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Function called when the manipulation is ended.
+    /// </summary>
+    /// <param name="gesture">The current gesture.</param>
+    protected override void OnEndManipulation(TapGesture gesture)
+    {
+        if (gesture.WasCancelled)
+        {
+            return;
+        }
+
+        // If gesture is targeting an existing object we are done.
+        if (gesture.TargetObject != null)
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon;
+
+        if (Frame.Raycast(gesture.StartPosition.x, gesture.StartPosition.y, raycastFilter, out hit))
+        {
+            // Use hit pose and camera pose to check if hittest is from the
+            // back of the plane, if it is, no need to create the anchor.
+            if ((hit.Trackable is DetectedPlane) &&
+                Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+                    hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                // Instantiate Andy model at the hit pose.
+                var andyObject = Instantiate(AndyPrefab, hit.Pose.position, hit.Pose.rotation);
+
+                // Instantiate manipulator.
+                var manipulator = Instantiate(ManipulatorPrefab, hit.Pose.position, hit.Pose.rotation);
+
+                // Make Andy model a child of the manipulator.
+                andyObject.transform.parent = manipulator.transform;
+
+                // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
+                // world evolves.
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                // Make manipulator a child of the anchor.
+                manipulator.transform.parent = anchor.transform;
+
+                // Select the placed object.
+                manipulator.GetComponent<Manipulator>().Select();
+            }
+        }
+    }
+}
